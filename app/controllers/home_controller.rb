73ecb6409bb19f4c3dcdc99d70b5cc65d1872281
abdurrehim <abdurrehim.sen@bil.omu.edu.tr>
2016-07-26 14:36:09 +0300

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	result = Cloudinary::Uploader.upload('http://www.neguzelsozler.com/wp-content/uploads/2013/07/resimli-ask-sozleri.jpg')
+	result = Cloudinary::Uploader.upload('http://www.neguzelsozler.com/wp-content/uploads/2013/07/resimli-ask-sozleri.jpg')
   	@url = result["url"]
 	@res2 = Clarifai::Rails::Detector.new(@url).image
 	
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'photo/index'
+
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
